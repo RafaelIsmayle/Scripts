308ed227ae0a8d03c0c48fde016e761e32d1ddd5
RafaelIsmayle <rafael.ismayle@gmail.com>
2020-11-30 17:33:03 -0300

    Update example.sh

--- bash/example.sh
+++ bash/example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Script de Teste"
 echo "Criando Arquivo de teste na Pasta TMP"
-touch arquivodeteste.txt /tmp/
+touch /tmp/arquivodeteste.txt
 echo "Esrevendo algo no arquivo"
 echo "algo escrito no dia $(date) " > /tmp/arquivodeteste.txt
 echo "Fim do Script"
